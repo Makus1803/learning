630b9ffc9da9a2067d6cce082ad16963e81074c9
Mateusz <mateusz.glocki@gmail.com>
2019-12-10 14:06:13 +0100

    add possibility to run with selected profile (mvn install -P "profileName") back-end request mapping

---
+++ src/main/java/com/organizr/web/pages/MainController.java
@@ -0,0 +1,12 @@
+package com.organizr.web.pages;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/login")
+    public String entry(){
+        return "index";
+    }
+}
